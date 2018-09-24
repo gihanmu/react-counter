9a443ccb7a713a7abb259b334968e6f9e63f5615
gihanmu <gihanubayawardana@gmail.com>
2018-09-24 23:33:56 +0530

    adding leaner switch statements

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,18 +5,18 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultId);
+    return updatedObject(state, {results: updatedArray})
+}
+
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
-        case actionTypes.STORE_RESULT:
-            return updatedObject(state, {results: state.results.concat({id: new Date(), value: action.result})})
-        
+        case actionTypes.STORE_RESULT: 
+         return updatedObject(state, {results: state.results.concat({id: new Date(), value: action.result})});        
 
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultId);
-            return updatedObject(state, {results: updatedArray})
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
+            return deleteResult(state, action);
           
     }
     return state;
